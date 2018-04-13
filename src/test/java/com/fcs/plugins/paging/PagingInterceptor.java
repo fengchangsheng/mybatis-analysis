2b10f8a7754b2c8a2acff49e3567c0812e677e12
Lucare <fengchangsheng123@qq.com>
2018-04-13 17:51:25 +0800

    真实分页代替伪分页

--- src/test/java/com/fcs/plugins/paging/PagingInterceptor.java
+++ src/test/java/com/fcs/plugins/paging/PagingInterceptor.java
@@ -27,7 +27,8 @@ public class PagingInterceptor implements Interceptor {
     private static final int mappedStatement_index = 0;
     private static final int paramObject_index = 1;
     private static final int rowBounds_index = 2;
-    private static final String count_table_alias = "COUNT_TABLE_OWN";
+    private static final String count_table_alias = "count_temp_table";
+    private static final String limit_table_alias = "limit_temp_table";
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -40,19 +41,26 @@ public class PagingInterceptor implements Interceptor {
         PagingRowBound pagingRowBound = (PagingRowBound) rowBoundsObject;
         MappedStatement mappedStatement = (MappedStatement) args[mappedStatement_index];
         Object paramObject = args[paramObject_index];
-//        BoundSql boundSql = mappedStatement.getBoundSql(paramObject);
-        BoundSql boundSql = mappedStatement.getSqlSource().getBoundSql(paramObject);
+        // 重写了一个SqlSource应该执行这个  后面这个就不需要了
+        BoundSql boundSql = mappedStatement.getBoundSql(paramObject);
+//        BoundSql boundSql = mappedStatement.getSqlSource().getBoundSql(paramObject);
         String sql = boundSql.getSql();
         logger.debug("> original sql: " + sql);
 //        String upperCaseSql = sql.toUpperCase();
-//        logger.debug("================= upperCaseSql ================>" + upperCaseSql);
         String countSql = "select count(*) from (" + sql + ") " + count_table_alias;
         Configuration configuration = mappedStatement.getConfiguration();
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
         // 构建新的查询组件（没有set方法又环环相扣）
         BoundSql newBoundSql = new BoundSql(configuration, countSql, parameterMappings, paramObject);
-        MappedStatement newMappedStatement = createMappedStatement(mappedStatement, newBoundSql);
+        MappedStatement newMappedStatement = createMappedStatementByCopy(mappedStatement, newBoundSql, true);
         Executor executor = (Executor) invocation.getTarget();
+        // 真实的分页操作
+        String limitSql = "select * from (" + sql + ") " + limit_table_alias + " limit " + pagingRowBound.getOffset() +
+                "," + pagingRowBound.getLimit();
+        BoundSql limitBoundSql = new BoundSql(configuration, limitSql, parameterMappings, paramObject);
+        MappedStatement limitMappedStatement = createMappedStatementByCopy(mappedStatement, limitBoundSql, false);
+        args[mappedStatement_index] = limitMappedStatement;
+        // 查询总记录数
         logger.debug("> invoke count select ");
         List<Integer> counts = executor.query(newMappedStatement, paramObject , new RowBounds(), null);
         if (counts != null && !counts.isEmpty()) {
@@ -65,19 +73,32 @@ public class PagingInterceptor implements Interceptor {
         return invocation.proceed();
     }
 
-    private MappedStatement createMappedStatement(MappedStatement oldMappedStatement, BoundSql newBoundSql){
-        MappedStatement.Builder msBuilder = new MappedStatement.Builder(oldMappedStatement.getConfiguration(),
-                oldMappedStatement.getId(), new SimpleSqlSource(newBoundSql),
-                oldMappedStatement.getSqlCommandType());
-        List<ResultMap> resultMaps = new ArrayList<>();
-        ResultMap.Builder inlineResultMapBuilder = new ResultMap.Builder(
+    private MappedStatement createMappedStatementByCopy(MappedStatement oldMappedStatement,
+                                                        BoundSql newBoundSql,
+                                                        boolean isQueryCount){
+        SqlSource sqlSource;
+        if (newBoundSql == null) {
+            sqlSource = oldMappedStatement.getSqlSource();
+        } else {
+            sqlSource = new SimpleSqlSource(newBoundSql);
+        }
+        MappedStatement.Builder msBuilder = new MappedStatement.Builder(
                 oldMappedStatement.getConfiguration(),
-                msBuilder.id() + "-Count-Inline",
-                Integer.class,
-                new ArrayList<ResultMapping>(),
-                null);
-        resultMaps.add(inlineResultMapBuilder.build());
-        msBuilder.resultMaps(resultMaps);
+                oldMappedStatement.getId(), sqlSource,
+                oldMappedStatement.getSqlCommandType());
+        if (isQueryCount) {
+            List<ResultMap> resultMaps = new ArrayList<>();
+            ResultMap.Builder inlineResultMapBuilder = new ResultMap.Builder(
+                    oldMappedStatement.getConfiguration(),
+                    msBuilder.id() + "-Count-Inline",
+                    Integer.class,
+                    new ArrayList<ResultMapping>(),
+                    null);
+            resultMaps.add(inlineResultMapBuilder.build());
+            msBuilder.resultMaps(resultMaps);
+        } else {
+            msBuilder.resultMaps(oldMappedStatement.getResultMaps());
+        }
         return msBuilder.build();
     }
 
@@ -95,7 +116,10 @@ public class PagingInterceptor implements Interceptor {
      * Executor执行query方法时会间接通过SqlSource来获取BoundSql :
      *   BoundSql boundSql = ms.getBoundSql(parameterObject);
      *
-     * 而原来的DynamicSqlSource#getBoundSql会触发一系列重新解析
+     * 而原来的DynamicSqlSource#getBoundSql会触发一系列动态解析，获取的时候已经不经意间完成了相关的操作。
+     * 后面框架的执行会再次调用mappedStatement来获取BoundSql，又要走一遍，而且这样你改了BoundSql也没用了。
+     * 唯一的办法就是重写个SqlSource，把修改过的BoundSql放进去，然后只是简单地提供一个get方法让后面使用这个BoundSql。
+     * 分页相关的操作都是基于原SQL的变种，不添加额外的参数，有参数也是直接写到sql中了，基本不用破坏其本身的结构。
      *
      * @see DynamicSqlSource
      */
